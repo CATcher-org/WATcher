73ae98b051c8c2b5db8951a44ae1bdb5c7d443f6
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2022-06-15 17:35:53 +0800

    Choose start and end dates

--- src/app/activity-dashboard/activity-dashboard.component.ts
+++ src/app/activity-dashboard/activity-dashboard.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import * as moment from 'moment';
 import { GithubUser } from '../core/models/github-user.model';
 import { GithubService } from '../core/services/github.service';
 import { GithubEventService } from '../core/services/githubevent.service';
 import { EXPANDED_TABLE_COLUMNS, TABLE_COLUMNS } from './event-tables/event-tables-columns';
-import { ACTION_BUTTONS } from './event-tables/event-tables.component';
+import { ACTION_BUTTONS, EventTablesComponent } from './event-tables/event-tables.component';
 
 @Component({
   selector: 'app-activity-dashboard',
@@ -15,12 +17,29 @@ export class ActivityDashboardComponent implements OnInit {
   readonly expandedColumns = [EXPANDED_TABLE_COLUMNS.ID, EXPANDED_TABLE_COLUMNS.TITLE, EXPANDED_TABLE_COLUMNS.DATE];
   readonly actionButtons: ACTION_BUTTONS[] = [ACTION_BUTTONS.VIEW_IN_WEB];
 
+  startMinDate: Date;
+  startMaxDate = moment().endOf('day').toDate();
+  endMinDate: Date;
+  endMaxDate = moment().endOf('day').toDate();
+
   assignees: GithubUser[];
+
+  @ViewChildren(EventTablesComponent) tables: QueryList<EventTablesComponent>;
 
   constructor(private githubService: GithubService, private githubEventService: GithubEventService) {}
 
   ngOnInit() {
     this.githubEventService.getEvents();
     this.githubService.getUsersAssignable().subscribe((x) => (this.assignees = x));
+  }
+
+  pickStartDate(event: MatDatepickerInputEvent<Date>) {
+    this.endMinDate = event.value;
+    this.tables.forEach((t) => (t.githubEvents.start = `${event.value}`));
+  }
+
+  pickEndDate(event: MatDatepickerInputEvent<Date>) {
+    this.startMaxDate = event.value;
+    this.tables.forEach((t) => (t.githubEvents.end = `${event.value}`));
   }
 }
--- src/app/activity-dashboard/activity-dashboard.module.ts
+++ src/app/activity-dashboard/activity-dashboard.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ActivityDashboardRoutingModule } from './activity-dashboard-routing.module';
 import { ActivityDashboardComponent } from './activity-dashboard.component';
 import { EventTablesModule } from './event-tables/event-tables.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ActivityDashboardComponent],
-  imports: [CommonModule, ActivityDashboardRoutingModule, EventTablesModule]
+  imports: [CommonModule, SharedModule, ActivityDashboardRoutingModule, EventTablesModule]
 })
 export class ActivityDashboardModule {}
