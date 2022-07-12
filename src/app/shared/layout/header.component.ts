40ffa99863a0e1df6e492e91418e900a43c3468b
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2022-07-12 15:25:29 +0800

    Build WATcher in Github workflows (#20)
    Let's fix WATcher Github workflows.

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -15,8 +15,6 @@ import { LoggingService } from '../../core/services/logging.service';
 import { PhaseDescription, PhaseService } from '../../core/services/phase.service';
 import { UserService } from '../../core/services/user.service';
 
-const ISSUE_TRACKER_URL = 'https://github.com/CATcher-org/CATcher/issues';
-
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html'
