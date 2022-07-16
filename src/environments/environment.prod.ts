86256e291e1c74423453d12b6f722cb0c78b7766
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2022-07-16 21:38:39 +0800

    Client ID of WATcher

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const AppConfig = {
   version: appSetting.version,
   production: true,
   test: false,
-  clientId: '5e1ed08cff7f0de1d68d',
+  clientId: 'dda4282bac3cac95148b', // Client ID sent to heroku gateway
   githubUrl: 'https://github.com',
   accessTokenUrl: 'https://catcher-auth.herokuapp.com/authenticate',
   clientDataUrl: 'https://raw.githubusercontent.com/CATcher-org/client_data/master/profiles.json',
