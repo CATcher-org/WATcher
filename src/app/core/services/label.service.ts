c25cfd0b3d19c92b96b69a9477a75326ee44467c
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2022-07-29 13:13:41 +0800

    Remove unused constants from Label Service

--- src/app/core/services/label.service.ts
+++ src/app/core/services/label.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, pipe, UnaryFunction } from 'rxjs';
-import { flatMap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Label } from '../models/label.model';
 import { GithubService } from './github.service';
 
@@ -13,150 +13,21 @@ const COLOR_DARKNESS_THRESHOLD = 0.184;
 const COLOR_BLACK = '000000'; // Dark color for text with light background
 const COLOR_WHITE = 'ffffff'; // Light color for text with dark background
 
-const COLOR_RED_PALE = 'ffe0e0';
-const COLOR_RED_LIGHT = 'ffcccc';
-const COLOR_RED = 'ff9999';
-const COLOR_RED_DARK = 'ff6666';
-
-const COLOR_PURPLE_LIGHT = 'd966ff';
-const COLOR_PURPLE = '9900cc';
-
-const COLOR_GREEN = '00802b';
-const COLOR_ORANGE_PALE = 'ffebcc';
-const COLOR_ORANGE_LIGHT = 'ffcc80';
-const COLOR_ORANGE = 'ff9900';
-
-const COLOR_SILVER = 'a6a6a6';
-
-const COLOR_BLUE = '0066ff';
-
-const DISPLAY_NAME_SEVERITY = 'Severity';
-const DISPLAY_NAME_BUG_TYPE = 'Bug Type';
-const DISPLAY_NAME_RESPONSE = 'Response';
-
-// The HTML template definition of selected labels are hard-coded here, move to a config file in the future
-const VERY_LOW_DEFINITION =
-  '<p>A flaw that is <mark>purely cosmetic</mark> and <mark>does not affect usage</mark>. For example, ' +
-  '<ul>' +
-  '<li>typo issues</li>' +
-  '<li>spacing issues</li>' +
-  '<li>layout issues</li>' +
-  '<li>color issues</li>' +
-  '<li>font issues</li>' +
-  '</ul>' +
-  "in the docs or the UI that doesn't affect usage.</p>";
-const LOW_DEFINITION =
-  '<p>A flaw that is unlikely to affect normal operations of the product. ' +
-  'Appears only in very rare situations and causes a minor inconvenience only.</p>';
-const MEDIUM_DEFINITION = '<p>A flaw that causes occasional inconvenience to some users but they can ' + 'continue to use the product.</p>';
-const HIGH_DEFINITION =
-  '<p>A flaw that affects most users and causes major problems for users.' + 'i.e., makes the product almost unusable for most users.</p>';
-
-const FUNCTIONALITY_BUG_DEFINITION = '<p>A functionality does not work as specified/expected.</p>';
-const FEATURE_FLAW_DEFINITION =
-  '<p>Some functionality missing from a feature delivered in the current version in ' +
-  'a way that the feature becomes less useful to the intended target user for <i>normal</i> usage. ' +
-  "i.e., the feature is not 'complete'.\nIn other words, an acceptance-testing bug that falls within " +
-  'the scope of the current version features. These issues are counted against the <i>product design</i> aspect ' +
-  'of the project.</p>';
-const DOCUMENTATION_BUG_DEFINITION =
-  '<p>A flaw in the documentation ' + '<span style="color:grey;">e.g., a missing step, a wrong instruction, typos</span></p>';
-
-const ACCEPTED_DEFINITION = '<p>You accept it as a bug.</p>';
-const NOT_IN_SCOPE_DEFINITION =
-  '<p>It is a valid issue but not something the team should be penalized for ' +
-  '<span style="color:grey;">e.g., it was not related to features delivered in this version</span>.</p>';
-const REJECTED_DEFINITION =
-  "<p>What tester treated as a bug is in fact the expected behavior (from the user's point of view), or the tester " +
-  'was mistaken in some other way.</p>';
-const CANNOT_REPRODUCE_DEFINITION = '<p>You are unable to reproduce the behavior reported in the bug after multiple tries.</p>';
-const ISSUE_UNCLEAR_DEFINITION = '<p>The issue description is not clear.</p>';
-const UNDEFINED_DEFINITION = null;
-
-export const LABEL_DEFINITIONS = {
-  severityVeryLow: VERY_LOW_DEFINITION,
-  severityLow: LOW_DEFINITION,
-  severityMedium: MEDIUM_DEFINITION,
-  severityHigh: HIGH_DEFINITION,
-  typeFunctionalityBug: FUNCTIONALITY_BUG_DEFINITION,
-  typeFeatureFlaw: FEATURE_FLAW_DEFINITION,
-  typeDocumentationBug: DOCUMENTATION_BUG_DEFINITION,
-  responseAccepted: ACCEPTED_DEFINITION,
-  responseNotInScope: NOT_IN_SCOPE_DEFINITION,
-  responseRejected: REJECTED_DEFINITION,
-  responseCannotProduce: CANNOT_REPRODUCE_DEFINITION,
-  responseIssueUnclear: ISSUE_UNCLEAR_DEFINITION,
-  undefined: UNDEFINED_DEFINITION
-};
-
-const REQUIRED_LABELS = {
-  severity: {
-    VeryLow: new Label('severity', 'VeryLow', COLOR_RED_PALE, VERY_LOW_DEFINITION),
-    Low: new Label('severity', 'Low', COLOR_RED_LIGHT, LOW_DEFINITION),
-    Medium: new Label('severity', 'Medium', COLOR_RED, MEDIUM_DEFINITION),
-    High: new Label('severity', 'High', COLOR_RED_DARK, HIGH_DEFINITION)
-  },
-  type: {
-    DocumentationBug: new Label('type', 'DocumentationBug', COLOR_PURPLE_LIGHT, DOCUMENTATION_BUG_DEFINITION),
-    FeatureFlaw: new Label('type', 'FeatureFlaw', COLOR_PURPLE_LIGHT, FEATURE_FLAW_DEFINITION),
-    FunctionalityBug: new Label('type', 'FunctionalityBug', COLOR_PURPLE, FUNCTIONALITY_BUG_DEFINITION)
-  },
-  response: {
-    Accepted: new Label('response', 'Accepted', COLOR_GREEN, ACCEPTED_DEFINITION),
-    CannotReproduce: new Label('response', 'CannotReproduce', COLOR_ORANGE_PALE, CANNOT_REPRODUCE_DEFINITION),
-    IssueUnclear: new Label('response', 'IssueUnclear', COLOR_ORANGE_LIGHT, ISSUE_UNCLEAR_DEFINITION),
-    NotInScope: new Label('response', 'NotInScope', COLOR_ORANGE_LIGHT, NOT_IN_SCOPE_DEFINITION),
-    Rejected: new Label('response', 'Rejected', COLOR_ORANGE, REJECTED_DEFINITION)
-  },
-  status: {
-    Done: new Label('status', 'Done', COLOR_SILVER),
-    Incomplete: new Label('status', 'Incomplete', COLOR_BLACK)
-  },
-  others: {
-    duplicate: new Label(undefined, 'duplicate', COLOR_BLUE)
-  }
-};
-
 @Injectable({
   providedIn: 'root'
 })
 
 /**
- * Responsible for retrieval and parsing and syncing of label data
+ * Responsible for retrieval and parsing of label data
  * from the GitHub repository for the WATcher application.
  */
 export class LabelService {
-  private static severityLabels: Label[] = Object.values(REQUIRED_LABELS.severity);
-  private static typeLabels: Label[] = Object.values(REQUIRED_LABELS.type);
-  private static responseLabels: Label[] = Object.values(REQUIRED_LABELS.response);
-  private static statusLabels: Label[] = Object.values(REQUIRED_LABELS.status);
-  private static otherLabels: Label[] = Object.values(REQUIRED_LABELS.others);
-  private static allLabelArrays = {
-    severity: LabelService.severityLabels,
-    type: LabelService.typeLabels,
-    response: LabelService.responseLabels,
-    status: LabelService.statusLabels,
-    others: LabelService.otherLabels
-  };
-  private static testerLabelArrays = {
-    severity: LabelService.severityLabels,
-    type: LabelService.typeLabels
-  };
-
   labels: Label[];
 
   constructor(private githubService: GithubService) {}
 
-  public static getRequiredLabelsAsArray(needAllLabels: boolean): Label[] {
-    let requiredLabels: Label[] = [];
-
-    const labels = needAllLabels ? Object.values(this.allLabelArrays) : Object.values(this.testerLabelArrays);
-    labels.map((label) => (requiredLabels = requiredLabels.concat(label)));
-    return requiredLabels;
-  }
-
   /**
-   * Fetch the labels in github.
+   * Fetch labels from Github.
    */
   public fetchLabels(): Observable<any> {
     return this.githubService.fetchAllLabels().pipe(
@@ -168,151 +39,14 @@ export class LabelService {
   }
 
   /**
-   * Returns an custom operator which helps to
-   * synchronise the labels in our application
-   * with the remote repository.
-   */
-  syncLabels(needAllLabels: boolean): UnaryFunction<Observable<boolean>, Observable<any>> {
-    return pipe(flatMap(() => this.synchronizeRemoteLabels(needAllLabels)));
-  }
-
-  /**
-   * Synchronizes the labels in github with those required by the application.
-   */
-  synchronizeRemoteLabels(needAllLabels: boolean): Observable<any> {
-    return this.githubService.fetchAllLabels().pipe(
-      map((response) => {
-        this.ensureRepoHasRequiredLabels(this.parseLabelData(response), LabelService.getRequiredLabelsAsArray(needAllLabels));
-        return response;
-      })
-    );
-  }
-
-  /**
-   * Returns all the labels of a certain type (e.g severity)
-   * @param attributeName: the type of the label
-   * @return an array of label of that type
-   */
-  getLabelList(attributeName: string): Label[] {
-    switch (attributeName) {
-      case 'severity':
-        return LabelService.severityLabels;
-      case 'type':
-        return LabelService.typeLabels;
-      case 'responseTag':
-      case 'response':
-        return LabelService.responseLabels;
-    }
-  }
-
-  /**
-   * Returns a title for the label type
-   * @param attributeName: the type of the label
-   */
-  getLabelTitle(attributeName: string): string {
-    switch (attributeName) {
-      case 'severity':
-        return DISPLAY_NAME_SEVERITY;
-      case 'type':
-        return DISPLAY_NAME_BUG_TYPE;
-      case 'responseTag':
-        return DISPLAY_NAME_RESPONSE;
-    }
-  }
-
-  /**
-   * Returns the color of the label by searching a list of
-   * all available labels.
-   * @param labelValue: the label's value (e.g Low / Medium / High / ...)
-   */
-  getColorOfLabel(labelValue: string): string {
-    // TODO: Rewrite function - labelValue insufficient to differentiate between labels. Should use `labelCategory.labelValue` format.
-    if (labelValue === '') {
-      return COLOR_WHITE;
-    }
-
-    const existingLabel = LabelService.getRequiredLabelsAsArray(true).find((label) => label.labelValue === labelValue);
-
-    if (existingLabel === undefined || existingLabel.labelColor === undefined) {
-      return COLOR_WHITE;
-    } else {
-      return existingLabel.labelColor;
-    }
-  }
-
-  /**
-   * Returns the definition of the label by searching a list of
-   * all available labels.
-   * @param labelValue: the label's value (e.g Low/ Medium/ High / ...).
-   * @param labelCategory: the label's category (e.g Type/ Severity / ...).
-   */
-  getLabelDefinition(labelValue: string, labelCategory: string): string {
-    if (labelValue === '' || labelValue === null || labelCategory === '' || labelCategory === null) {
-      return null;
-    }
-
-    const existingLabel = LabelService.getRequiredLabelsAsArray(true).find(
-      (label) => label.labelValue === labelValue && label.labelCategory === labelCategory
-    );
-
-    if (existingLabel === undefined || existingLabel.labelDefinition === undefined) {
-      return null;
-    } else {
-      return existingLabel.labelDefinition;
-    }
-  }
-
-  /**
-   * Ensures that the repo has the required labels.
-   * Compares the actual labels in the repo with the required labels. If an required label is missing,
-   * it is added to the repo. If the required label exists but the label color is not as expected,
-   * the color is updated. Does not delete actual labels that do not match required labels.
-   * i.e., the repo might have more labels than the required labels after this operation.
-   * @param actualLabels: labels in the repo.
-   * @param requiredLabels: required labels.
-   */
-  private ensureRepoHasRequiredLabels(actualLabels: Label[], requiredLabels: Label[]): void {
-    requiredLabels.forEach((label) => {
-      // Finds for a label that has the same name as a required label.
-      const nameMatchedLabels: Label[] = actualLabels.filter((remoteLabel) => remoteLabel.getFormattedName() === label.getFormattedName());
-
-      if (nameMatchedLabels.length === 0) {
-        // Create new Label (Could not find a label with the same name & category)
-        this.githubService.createLabel(label.getFormattedName(), label.labelColor);
-      } else if (nameMatchedLabels.length === 1) {
-        if (nameMatchedLabels[0].equals(label)) {
-          // the label exists exactly as expected -> do nothing
-        } else {
-          // the label exists but the color does not match
-          this.githubService.updateLabel(label.getFormattedName(), label.labelColor);
-        }
-      } else {
-        throw new Error('Unexpected error: the repo has multiple labels with the same name ' + label.getFormattedName());
-      }
-    });
-  }
-
-  /**
    * Parses label information and returns an array of Label objects.
    * @param labels - Label Information from API.
    */
-  parseLabelData(labels: Array<{}>): Label[] {
+  parseLabelData(labels: Array<any>): Label[] {
     const labelData: Label[] = [];
 
     for (const label of labels) {
-      let labelCategory: string;
-      let labelValue: string;
-      const containsDotRegex = /\./g;
-
-      const rawName: string = String(label['name']);
-
-      [labelCategory, labelValue] = containsDotRegex.test(rawName) ? String(label['name']).split('.') : [undefined, rawName];
-
-      const labelColor: string = String(label['color']);
-
-      const labelDefinition: string = String(label['definition']);
-
-      labelData.push(new Label(labelCategory, labelValue, labelColor, labelDefinition));
+      labelData.push(new Label(label));
     }
     return labelData;
   }
