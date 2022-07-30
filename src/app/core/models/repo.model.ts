d445df7d5a5397d14778eacd645ba6e4e3eec1f8
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2022-07-30 17:30:01 +0800

    Add repo model comments

--- src/app/core/models/repo.model.ts
+++ src/app/core/models/repo.model.ts
@@ -6,11 +6,13 @@ export class Repo {
   owner: string;
   name: string;
 
+  /** Creates a new Repo from owner and name strings. */
   constructor(owner: string, name: string) {
     this.owner = owner;
     this.name = name;
   }
 
+  /** Creates a new Repo from one repository url. */
   public static of(repoUrl: string) {
     const repoUrlSplit = repoUrl.split('/');
     if (repoUrlSplit.length !== 2) {
@@ -19,6 +21,7 @@ export class Repo {
     return new Repo(repoUrlSplit[0], repoUrlSplit[1]);
   }
 
+  /** String representation of a Repo. */
   public toString(): string {
     return this.owner + '/' + this.name;
   }
