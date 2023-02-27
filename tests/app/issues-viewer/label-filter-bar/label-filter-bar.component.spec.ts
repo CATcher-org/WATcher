b0b2636101825ac25da831d7f10032177f4b5e8b
ZQ <e0725180@u.nus.edu>
2023-02-27 13:09:00 +0800

    Removes unused constants

--- tests/app/issues-viewer/label-filter-bar/label-filter-bar.component.spec.ts
+++ tests/app/issues-viewer/label-filter-bar/label-filter-bar.component.spec.ts
@@ -2,32 +2,32 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LabelFilterBarComponent, simplifiedLabel } from '../../../../src/app/issues-viewer/label-filter-bar/label-filter-bar.component';
 
-const dummyDataLabels: simplifiedLabel[] = [
-  {
-    name: 'dummy1',
-    color: '#2A6478'
-  },
-  {
-    name: 'dummy2',
-    color: '#705335'
-  },
-  {
-    name: 'dummy3',
-    color: '#6D3F5B'
-  },
-  {
-    name: 'dummy4',
-    color: '#587246'
-  },
-  {
-    name: 'dummy5',
-    color: '#FF7514'
-  },
-  {
-    name: 'dummy6',
-    color: '#1D334A'
-  }
-];
+// const dummyDataLabels: simplifiedLabel[] = [
+//   {
+//     name: 'dummy1',
+//     color: '#2A6478'
+//   },
+//   {
+//     name: 'dummy2',
+//     color: '#705335'
+//   },
+//   {
+//     name: 'dummy3',
+//     color: '#6D3F5B'
+//   },
+//   {
+//     name: 'dummy4',
+//     color: '#587246'
+//   },
+//   {
+//     name: 'dummy5',
+//     color: '#FF7514'
+//   },
+//   {
+//     name: 'dummy6',
+//     color: '#1D334A'
+//   }
+// ];
 
 describe('LabelFilterBarComponent', () => {
   let component: LabelFilterBarComponent;
