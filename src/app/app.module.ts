0e4d348f5884dd3369ae5cb16effcd37af46fe41
Echomo Xinyu <seanxinyu2@gmail.com>
2023-03-18 15:23:06 +0800

    Revert change for entryComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -96,7 +96,8 @@ import { SharedModule } from './shared/shared.module';
       useClass: ErrorHandlingService
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [UserConfirmationComponent, SessionFixConfirmationComponent, LabelDefinitionPopupComponent, RepoChangeFormComponent]
 })
 export class AppModule {
   constructor(private apollo: Apollo, private httpLink: HttpLink, private authService: AuthService, private logger: LoggingService) {
