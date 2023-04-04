7caa88077103124e06fcc18eedf43305916d7d18
ZQ <e0725180@u.nus.edu>
2023-04-04 19:12:39 +0800

    Update IssueList.ts

--- src/app/shared/issue-tables/IssueList.ts
+++ src/app/shared/issue-tables/IssueList.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, merge, Observable } from 'rxjs';
+import { BehaviorSubject, merge, Observable, Subscription } from 'rxjs';
 import { Issue } from '../../core/models/issue.model';
 import { DataSource } from '@angular/cdk/collections';
 import { searchFilter } from './search-filter';
@@ -6,7 +6,8 @@ import { TABLE_COLUMNS } from './issue-tables-columns';
 import { DEFAULT_DROPDOWN_FILTER, DropdownFilter, applyDropdownFilter } from './dropdownfilter';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
-import { map } from 'rxjs/operators';
+import { getSortedData } from './issue-sorter';
+import { paginateData } from './issue-paginator';
 
 /**
  * Class similar to IssueDataTable but instead of listening to issueService,
@@ -14,28 +15,25 @@ import { map } from 'rxjs/operators';
  */
 export class IssueList extends DataSource<Issue> {
   readonly DISPLAYEDCOLUMN = [TABLE_COLUMNS.ID, TABLE_COLUMNS.TITLE, TABLE_COLUMNS.ASSIGNEE, TABLE_COLUMNS.LABEL];
-  private issues: Issue[];
   private observableIssues = new BehaviorSubject<Issue[]>([]);
   private _dropdownFilter = new BehaviorSubject<DropdownFilter>(DEFAULT_DROPDOWN_FILTER);
   private _searchFilter = new BehaviorSubject<string>('');
+  private issueSubscription: Subscription;
   public count = 0;
 
   constructor(private issues$: BehaviorSubject<Issue[]>, private sort: MatSort, private paginator: MatPaginator) {
     super();
   }
 
-  loadIssue() {
-    this.issues$.subscribe((items) => {
-      this.issues = items;
-      this.count = items.length;
-    });
-  }
-
   connect(): Observable<Issue[] | readonly Issue[]> {
     return this.observableIssues.asObservable();
   }
+
   disconnect(): void {
+    this._dropdownFilter.complete();
+    this._searchFilter.complete();
     this.observableIssues.complete();
+    this.issueSubscription.unsubscribe();
   }
 
   get dropdownFilter(): DropdownFilter {
@@ -54,14 +52,23 @@ export class IssueList extends DataSource<Issue> {
     this._searchFilter.next(newFilter);
   }
 
-  initialize(): void {
+  loadData(): void {
     const displayDataChanges = [this.issues$, this.paginator?.page, this.sort?.sortChange, this._dropdownFilter, this._searchFilter].filter(
       (x) => x !== undefined
     );
 
-    merge(...displayDataChanges).subscribe(() => {
+    this.issueSubscription = merge(...displayDataChanges).subscribe(() => {
       let data = this.issues$.getValue();
-      data = data.filter(searchFilter(this.searchFilter, this.DISPLAYEDCOLUMN)).filter(applyDropdownFilter(this.dropdownFilter));
+      this.count = data.length;
+      data = data.filter(searchFilter(this.searchFilter, this.DISPLAYEDCOLUMN));
+      data = data.filter(applyDropdownFilter(this.dropdownFilter));
+      if (this.sort) {
+        data = getSortedData(this.sort, data);
+      }
+      if (this.paginator) {
+        data = paginateData(this.paginator, data);
+      }
+      this.issues$.next(data);
     });
   }
 }
