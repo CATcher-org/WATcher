81c7b3b54b3e27c0f0f78e3eb8055f2f355ad83a
ZQ <e0725180@u.nus.edu>
2023-04-06 15:39:47 +0800

    Recover lost files

--- src/app/detailed-viewer/detailed-viewer.module.ts
+++ src/app/detailed-viewer/detailed-viewer.module.ts
@@ -10,9 +10,17 @@ import { ProfileActivitiesComponent } from './profile-activities/profile-activit
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 import { ProfileIconComponent } from './profile-icon/profile-icon.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 
 @NgModule({
   imports: [DetailedViewerRoutingModule, FilterBarModule, SharedModule, IssuesPrCardModule, IssuesViewerModule, MarkdownModule.forChild()],
-  declarations: [DetailedViewerComponent, ProfileIconComponent, ProfileDetailsComponent, ProfileActivitiesComponent, ProfileListComponent]
+  declarations: [
+    DetailedViewerComponent,
+    ProfileIconComponent,
+    ProfileDetailsComponent,
+    ProfileActivitiesComponent,
+    ProfileListComponent,
+    CalendarViewComponent
+  ]
 })
 export class DetailedViewerModule {}
