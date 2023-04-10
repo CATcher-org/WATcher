7dd356d37f8cda5592467ec565b255fcf1c80f58
lzq <li.zhaoqi@dhs.sg>
2023-04-10 19:45:23 +0800

    Shift files to a new PR

--- src/app/detailed-viewer/detailed-viewer.module.ts
+++ src/app/detailed-viewer/detailed-viewer.module.ts
@@ -10,17 +10,9 @@ import { DetailedViewerComponent } from './detailed-viewer.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 import { ProfileIconComponent } from './profile-icon/profile-icon.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
-import { DateRangeDialogComponent } from './query-range/query-range.component';
 
 @NgModule({
   imports: [DetailedViewerRoutingModule, FilterBarModule, SharedModule, IssuesPrCardModule, IssuesViewerModule, MarkdownModule.forChild()],
-  declarations: [
-    DetailedViewerComponent,
-    ProfileIconComponent,
-    ProfileDetailsComponent,
-    ProfileListComponent,
-    CommitListComponent,
-    DateRangeDialogComponent
-  ]
+  declarations: [DetailedViewerComponent, ProfileIconComponent, ProfileDetailsComponent, ProfileListComponent, CommitListComponent]
 })
 export class DetailedViewerModule {}
