55ed5055e9bf85de945d482e76271ba65cca8df8
lzq <li.zhaoqi@dhs.sg>
2023-04-10 20:06:49 +0800

    Moves more files to another PR

--- src/app/detailed-viewer/commit-list/commit-list.component.ts
+++ src/app/detailed-viewer/commit-list/commit-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Moment } from 'moment';
-import { Accumulator, PrefixSum } from '../../core/models/datastructure/rsq.model';
 import { GithubCommit } from '../../core/models/github/github-commit.model';
 import { DAY_IN_MILISECOND, toMaxTime, toMinTime } from '../datetimehelper';
 
@@ -13,29 +12,6 @@ import { DAY_IN_MILISECOND, toMaxTime, toMinTime } from '../datetimehelper';
 export interface DateRange {
   minDate: Moment;
   maxDate: Moment;
-}
-class CumulativeStats implements Accumulator<CumulativeStats> {
-  additions: number;
-  deletions: number;
-  count: number;
-
-  constructor(commmit?: GithubCommit) {
-    this.additions = commmit?.additions ?? 0;
-    this.deletions = commmit?.deletions ?? 0;
-    this.count = commmit ? 1 : 0;
-  }
-
-  add(o: CumulativeStats): void {
-    this.additions += o.additions;
-    this.deletions += o.deletions;
-    this.count += o.count;
-  }
-
-  sub(o: CumulativeStats): void {
-    this.additions -= o.additions;
-    this.deletions -= o.deletions;
-    this.count -= o.count;
-  }
 }
 
 @Component({
@@ -52,7 +28,6 @@ export class CommitListComponent implements OnInit {
   // this will be displayed
   commits: GithubCommit[];
 
-  liveStats: CumulativeStats;
   dateRange: DateRange = { maxDate: undefined, minDate: undefined };
 
   step = -1;
@@ -60,7 +35,6 @@ export class CommitListComponent implements OnInit {
   startIndex = 1;
   endIndex: number;
 
-  ps: PrefixSum<CumulativeStats>;
   private firstTime: Moment;
 
   constructor(public dialog: MatDialog) {}
@@ -82,7 +56,6 @@ export class CommitListComponent implements OnInit {
 
   createPrefixSum() {
     if (this.commitList.length === 0) {
-      this.ps = new PrefixSum([], () => new CumulativeStats());
       return;
     }
     this.firstTime = toMinTime(this.commitList[0].committedDate);
@@ -90,15 +63,6 @@ export class CommitListComponent implements OnInit {
     const commitToIndex = (commit: GithubCommit) => this.dateToIndex(commit.committedDate);
 
     this.endIndex = commitToIndex(this.commitList[this.commitList.length - 1]);
-    const prefixArr = new Array(this.endIndex + 1);
-    for (let i = 0; i < prefixArr.length; i++) {
-      prefixArr[i] = new CumulativeStats();
-    }
-    for (const commit of this.commitList) {
-      prefixArr[commitToIndex(commit)].add(new CumulativeStats(commit));
-    }
-
-    this.ps = new PrefixSum(prefixArr, () => new CumulativeStats());
   }
 
   // Convert date to index of commitList
@@ -112,7 +76,6 @@ export class CommitListComponent implements OnInit {
 
     if (!sDate || !eDate) {
       this.commits = [];
-      this.liveStats = new CumulativeStats();
       return;
     }
     // close all opened commits
@@ -129,6 +92,5 @@ export class CommitListComponent implements OnInit {
     if (this.latestToLast) {
       this.commits.reverse();
     }
-    this.liveStats = this.ps.rsq(this.startIndex, this.endIndex);
   }
 }
