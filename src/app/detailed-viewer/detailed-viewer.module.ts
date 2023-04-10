4cba9fcd6248741f23773f2fb28b3afd005c6d07
lzq <li.zhaoqi@dhs.sg>
2023-04-10 19:41:08 +0800

    Fix linting

--- src/app/detailed-viewer/detailed-viewer.module.ts
+++ src/app/detailed-viewer/detailed-viewer.module.ts
@@ -7,8 +7,8 @@ import { SharedModule } from '../shared/shared.module';
 import { CommitListComponent } from './commit-list/commit-list.component';
 import { DetailedViewerRoutingModule } from './detailed-viewer-routing.module';
 import { DetailedViewerComponent } from './detailed-viewer.component';
-import { ProfileIconComponent } from './profile-icon/profile-icon.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { ProfileIconComponent } from './profile-icon/profile-icon.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
 import { DateRangeDialogComponent } from './query-range/query-range.component';
 
