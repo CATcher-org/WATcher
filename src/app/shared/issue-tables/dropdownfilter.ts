97405d34ad494da71613356d056564cbd529b6d7
lzq <li.zhaoqi@dhs.sg>
2023-04-14 14:29:35 +0800

    adds additional comments

--- src/app/shared/issue-tables/dropdownfilter.ts
+++ src/app/shared/issue-tables/dropdownfilter.ts
@@ -30,6 +30,7 @@ export function applyDropdownFilter(dropdownFilter: DropdownFilter): (a: Issue) 
     if (dropdownFilter.status === 'open') {
       ret = ret && issue.state === 'OPEN';
     } else if (dropdownFilter.status === 'closed') {
+      // there is apparently also a status called 'all' based on github api
       ret = ret && issue.state === 'CLOSED';
     }
 
