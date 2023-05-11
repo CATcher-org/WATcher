425bbf990c4e2c33b75bffaeb1f80114213f7c9d
ZQ <e0725180@u.nus.edu>
2023-05-12 07:32:01 +0800

    Renames component name for new page

--- src/app/detailed-viewer/user-detail-viewer.module.ts
+++ src/app/detailed-viewer/user-detail-viewer.module.ts
@@ -4,13 +4,13 @@ import { IssuesViewerModule } from '../issues-viewer/issues-viewer.module';
 import { FilterBarModule } from '../shared/filter-bar/filter-bar.module';
 import { IssuesPrCardModule } from '../shared/issue-pr-card/issue-pr-card.module';
 import { SharedModule } from '../shared/shared.module';
-import { DetailedViewerRoutingModule } from './detailed-viewer-routing.module';
-import { DetailedViewerComponent } from './detailed-viewer.component';
 import { ProfileIconComponent } from './profile-icon/profile-icon.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
+import { DetailedViewerRoutingModule } from './user-detail-viewer-routing.module';
+import { UserDetailViewerComponent } from './user-detail-viewer.component';
 
 @NgModule({
   imports: [DetailedViewerRoutingModule, FilterBarModule, SharedModule, IssuesPrCardModule, IssuesViewerModule, MarkdownModule.forChild()],
-  declarations: [DetailedViewerComponent, ProfileIconComponent, ProfileListComponent]
+  declarations: [UserDetailViewerComponent, ProfileIconComponent, ProfileListComponent]
 })
 export class DetailedViewerModule {}
