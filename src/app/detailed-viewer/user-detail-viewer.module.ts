e2207300ccab9d00c0ffb283aa9208f86000c212
ZQ <e0725180@u.nus.edu>
2023-05-12 07:32:01 +0800

    Renames component name for new page

--- src/app/detailed-viewer/user-detail-viewer.module.ts
+++ src/app/detailed-viewer/user-detail-viewer.module.ts
@@ -5,14 +5,14 @@ import { FilterBarModule } from '../shared/filter-bar/filter-bar.module';
 import { IssuesPrCardModule } from '../shared/issue-pr-card/issue-pr-card.module';
 import { SharedModule } from '../shared/shared.module';
 import { CommitListComponent } from './commit-list/commit-list.component';
-import { DetailedViewerRoutingModule } from './detailed-viewer-routing.module';
-import { DetailedViewerComponent } from './detailed-viewer.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 import { ProfileIconComponent } from './profile-icon/profile-icon.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
+import { DetailedViewerRoutingModule } from './user-detail-viewer-routing.module';
+import { UserDetailViewerComponent } from './user-detail-viewer.component';
 
 @NgModule({
   imports: [DetailedViewerRoutingModule, FilterBarModule, SharedModule, IssuesPrCardModule, IssuesViewerModule, MarkdownModule.forChild()],
-  declarations: [DetailedViewerComponent, ProfileIconComponent, ProfileDetailsComponent, ProfileListComponent, CommitListComponent]
+  declarations: [UserDetailViewerComponent, ProfileIconComponent, ProfileListComponent]
 })
 export class DetailedViewerModule {}
