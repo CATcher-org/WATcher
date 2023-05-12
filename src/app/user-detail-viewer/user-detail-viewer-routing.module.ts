2f19ad4309c135216c58dfd9c646ee860b547f60
ZQ <e0725180@u.nus.edu>
2023-05-12 09:22:52 +0800

    Renames user page router

--- src/app/user-detail-viewer/user-detail-viewer-routing.module.ts
+++ src/app/user-detail-viewer/user-detail-viewer-routing.module.ts
@@ -12,4 +12,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DetailedViewerRoutingModule {}
+export class UserDetailViewerRoutingModule {}
--- src/app/user-detail-viewer/user-detail-viewer.module.ts
+++ src/app/user-detail-viewer/user-detail-viewer.module.ts
@@ -5,11 +5,18 @@ import { FilterBarModule } from '../shared/filter-bar/filter-bar.module';
 import { IssuesPrCardModule } from '../shared/issue-pr-card/issue-pr-card.module';
 import { SharedModule } from '../shared/shared.module';
 import { ProfileIconComponent } from './profile-icon/profile-icon.component';
-import { DetailedViewerRoutingModule } from './user-detail-viewer-routing.module';
+import { UserDetailViewerRoutingModule } from './user-detail-viewer-routing.module';
 import { UserDetailViewerComponent } from './user-detail-viewer.component';
 
 @NgModule({
-  imports: [DetailedViewerRoutingModule, FilterBarModule, SharedModule, IssuesPrCardModule, IssuesViewerModule, MarkdownModule.forChild()],
+  imports: [
+    UserDetailViewerRoutingModule,
+    FilterBarModule,
+    SharedModule,
+    IssuesPrCardModule,
+    IssuesViewerModule,
+    MarkdownModule.forChild()
+  ],
   declarations: [UserDetailViewerComponent, ProfileIconComponent]
 })
 export class DetailedViewerModule {}
