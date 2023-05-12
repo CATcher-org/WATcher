6ee938cce3f663c65f3e63a09b7de74cadc85faf
ZQ <e0725180@u.nus.edu>
2023-05-12 09:01:21 +0800

    Fix missing phase redirection

--- src/app/detailed-viewer/user-detail-viewer-routing.module.ts
+++ src/app/detailed-viewer/user-detail-viewer-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../core/guards/auth.guard';
 import { UserDetailViewerComponent } from './user-detail-viewer.component';
 
-const routes: Routes = [{ path: 'user/:name', component: UserDetailViewerComponent, canActivate: [AuthGuard] }];
+const routes: Routes = [
+  { path: 'user/:name', component: UserDetailViewerComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserDetailViewerComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
