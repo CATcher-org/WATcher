dc88c7937ccf0d04149eb28a00db420adcd005ef
Joy Ng Jing Ru <e0943052@u.nus.edu>
2023-07-22 23:34:48 +0800

    Import necessary components

--- tests/app/auth/confirm-login/confirm-login.component.spec.ts
+++ tests/app/auth/confirm-login/confirm-login.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
 import { ConfirmLoginComponent } from '../../../../src/app/auth/confirm-login/confirm-login.component';
