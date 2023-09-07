c57d18ef4f1df1c2c3512589ef4bff7371705665
wweqg <e0726629@u.nus.edu>
2023-09-07 15:55:01 +0800

    fix warning

--- tests/app/auth/confirm-login/confirm-login.component.spec.ts
+++ tests/app/auth/confirm-login/confirm-login.component.spec.ts
@@ -1,4 +1,4 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
 import { ConfirmLoginComponent } from '../../../../src/app/auth/confirm-login/confirm-login.component';
