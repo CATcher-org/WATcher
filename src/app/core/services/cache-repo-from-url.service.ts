ac05ea040b60b6262e918b70ebbe1e02129db407
LJXSean <risingsun221@gmail.com>
2023-09-19 00:30:35 +0800

    Create service to cache repoLocation

---
+++ src/app/core/services/cache-repo-from-url.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheRepoFromUrlService {
+  static readonly KEY_NAME = 'repoLocation';
+
+  get repoLocation() {
+    const cachedRepoLocation = sessionStorage.getItem(CacheRepoFromUrlService.KEY_NAME) || null;
+    return cachedRepoLocation;
+  }
+
+  set repoLocation(repoLocationString: string) {
+    sessionStorage.setItem(CacheRepoFromUrlService.KEY_NAME, repoLocationString);
+  }
+
+  hasRepoLocation() {
+    return this.repoLocation != null;
+  }
+}
