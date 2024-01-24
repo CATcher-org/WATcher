d22d9f634953911f74c9b103c20eb224128850a5
Arif <arifkhalid99@gmail.com>
2024-01-24 20:26:52 +0800

    Add comments in FiltersStore

--- src/app/shared/issue-tables/FiltersStore.ts
+++ src/app/shared/issue-tables/FiltersStore.ts
@@ -2,21 +2,18 @@ import { DEFAULT_DROPDOWN_FILTER, DropdownFilter } from './dropdownfilter';
 
 const STORED_FILTER_PROPERTIES = ['status', 'type', 'sort', 'sortDirection'];
 
+/** This static class stores the filters applied for the purpose of saving filters across repo changes */
 export class FiltersStore {
-  /** This copy of the filter is constantly updated when a change in the drop down filter is detected */
+  /** This copy of the dropdown filter is constantly updated when a change in the drop down filter occurs */
   private static currentDropdownFilter: DropdownFilter = { ...DEFAULT_DROPDOWN_FILTER };
 
+  /** This copy of the search filter is constantly updated when a change in search filter occurs*/
   private static currentSearchFilter = '';
 
   static updateDropdownFilter(dropdownFilter: DropdownFilter) {
     for (const property of STORED_FILTER_PROPERTIES) {
       this.currentDropdownFilter[property] = dropdownFilter[property];
     }
-  }
-
-  static clearStoredFilters() {
-    this.currentDropdownFilter = { ...DEFAULT_DROPDOWN_FILTER };
-    this.currentSearchFilter = '';
   }
 
   static getInitialDropdownFilter(): DropdownFilter {
@@ -29,5 +26,10 @@ export class FiltersStore {
 
   static getInitialSearchFilter(): string {
     return this.currentSearchFilter.slice();
+  }
+
+  static clearStoredFilters() {
+    this.currentDropdownFilter = { ...DEFAULT_DROPDOWN_FILTER };
+    this.currentSearchFilter = '';
   }
 }
