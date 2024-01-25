45329f9f493f9b3f23a9279f4a23f38dda5a24a6
Arif <arifkhalid99@gmail.com>
2024-01-25 15:44:21 +0800

    Remove unused import

--- src/app/issues-viewer/card-view/card-view.component.ts
+++ src/app/issues-viewer/card-view/card-view.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostBinding, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Observable } from 'rxjs';
