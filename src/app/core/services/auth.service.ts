93c1a1faf785e04acb5f1b8f37d3cc5ae089523a
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-02-20 21:24:41 +0800

    Update constant name

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -33,7 +33,7 @@ export enum AuthState {
  * updating the application state with regards to authentication.
  */
 export class AuthService {
-  private static readonly DEFAULT_HAS_PERMISSION_TO_PRIVATE_REPOS = false;
+  private static readonly NO_PERMISSION_TO_PRIVATE_REPOS = false;
   private static readonly SESSION_NEXT_KEY = 'next';
 
   authStateSource = new BehaviorSubject(AuthState.NotAuthenticated);
@@ -79,7 +79,7 @@ export class AuthService {
   startOAuthIfHasNext() {
     if (this.hasNext()) {
       this.logger.info(`AuthService: Start OAuth because there is a next route`);
-      this.startOAuthProcess(AuthService.DEFAULT_HAS_PERMISSION_TO_PRIVATE_REPOS);
+      this.startOAuthProcess(AuthService.NO_PERMISSION_TO_PRIVATE_REPOS);
     }
   }
 
