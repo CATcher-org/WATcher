80458817c5317c9204a623525d297bfa3a1706c8
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2024-02-22 17:26:43 +0800

    Add assertion for all properties

--- tests/model/issue.model.spec.ts
+++ tests/model/issue.model.spec.ts
@@ -1,15 +1,32 @@
 import * as moment from 'moment';
 import { Issue } from '../../src/app/core/models/issue.model';
+import { Milestone } from '../../src/app/core/models/milestone.model';
 import { ISSUE_WITH_ASSIGNEES, ISSUE_WITH_EMPTY_DESCRIPTION } from '../constants/githubissue.constants';
 
 describe('Issue model class', () => {
   describe('.createPhaseBugReportIssue(githubIssue)', () => {
     it('correctly creates a bug reporting issue that has an empty description', async () => {
       const issue = Issue.createPhaseBugReportingIssue(ISSUE_WITH_EMPTY_DESCRIPTION);
-      expect(issue.title).toBe('App starts to lag when given large amount of input');
-      expect(issue.description).toBe('No details provided by bug reporter.');
-      expect(issue.updated_at).toBe(moment(ISSUE_WITH_EMPTY_DESCRIPTION.updated_at).format('lll'));
-      expect(issue.state).toBe(ISSUE_WITH_EMPTY_DESCRIPTION.state);
+      expect(issue.globalId).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.id);
+      expect(issue.id).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.number);
+      expect(issue.created_at).toEqual(moment(ISSUE_WITH_EMPTY_DESCRIPTION.created_at).format('lll'));
+      expect(issue.title).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.title);
+      expect(issue.description).toEqual('No details provided by bug reporter.');
+      expect(issue.updated_at).toEqual(moment(ISSUE_WITH_EMPTY_DESCRIPTION.updated_at).format('lll'));
+      expect(issue.closed_at).toEqual(moment(ISSUE_WITH_EMPTY_DESCRIPTION.closed_at).format('lll'));
+      expect(issue.milestone).toEqual(
+        ISSUE_WITH_EMPTY_DESCRIPTION.milestone ? new Milestone(ISSUE_WITH_EMPTY_DESCRIPTION.milestone) : Milestone.DefaultMilestone
+      );
+      expect(issue.state).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.state);
+      expect(issue.stateReason).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.stateReason);
+      if (ISSUE_WITH_EMPTY_DESCRIPTION.issueOrPr) {
+        expect(issue.issueOrPr).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.issueOrPr);
+      }
+      expect(issue.author).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.user.login);
+      expect(issue.isDraft).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.isDraft);
+      expect(issue.assignees).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.assignees.map((assignee) => assignee.login));
+      expect(issue.labels).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.labels.map((label) => label.name));
+      expect(issue.githubLabels).toEqual(ISSUE_WITH_EMPTY_DESCRIPTION.labels);
     });
   });
   describe('.updateDescription(description)', () => {
