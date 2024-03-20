d7f44cd30fede98a22d003e570fa2e3cc2d2039e
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-03-20 20:16:38 +0800

    Update label filter bar

--- src/app/shared/filter-bar/label-filter-bar/label-filter-bar.component.ts
+++ src/app/shared/filter-bar/label-filter-bar/label-filter-bar.component.ts
@@ -34,7 +34,6 @@ export class LabelFilterBarComponent implements OnInit, AfterViewInit, OnDestroy
       this.labels$ = this.labelService.connect();
       this.labels$.subscribe((labels) => {
         this.allLabels = labels;
-        this.filtersService.sanitizeLabels(this.allLabels);
         this.selectedLabelNames = this.filtersService.filter$.value.labels;
         this.hiddenLabelNames = this.filtersService.filter$.value.hiddenLabels;
       });
