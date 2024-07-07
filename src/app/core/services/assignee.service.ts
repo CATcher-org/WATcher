79446cecf60368754d5b66b4b26e3f795de296d5
kokerinks <limkokliang01@gmail.com>
2024-07-07 15:23:36 +0800

    Add assignee service

---
+++ src/app/core/services/assignee.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GithubUser } from '../models/github-user.model';
+import { GithubService } from './github.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssigneeService {
+  assignees: GithubUser[] = [];
+  hasNoAssignees: boolean;
+
+  constructor(private githubService: GithubService) {}
+
+  /**
+   * Fetch all assignees from github.
+   */
+  public fetchAssignees(): Observable<any> {
+    return this.githubService.getUsersAssignable().pipe(
+      map((response) => {
+        this.assignees = this.parseAssigneeData(response);
+        this.hasNoAssignees = response.length === 0;
+        return response;
+      })
+    );
+  }
+
+  /**
+   * Parses assignee information and returns an array of GithubUser objects.
+   * @param assignees - Assignee Information from API.
+   */
+  parseAssigneeData(assignees: Array<any>): GithubUser[] {
+    const assigneeData: GithubUser[] = [];
+
+    for (const assignee of assignees) {
+      assigneeData.push(new GithubUser(assignee));
+    }
+    assigneeData.sort((a: GithubUser, b: GithubUser) => a.login.localeCompare(b.login));
+
+    return assigneeData;
+  }
+}
