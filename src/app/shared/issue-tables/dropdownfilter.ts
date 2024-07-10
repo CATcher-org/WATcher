bc485627277d1371599a22d726771e8ab3937b96
kokerinks <limkokliang01@gmail.com>
2024-07-10 17:59:08 +0800

    Fix linting and tests

--- src/app/shared/issue-tables/dropdownfilter.ts
+++ src/app/shared/issue-tables/dropdownfilter.ts
@@ -52,18 +52,18 @@ export function applyDropdownFilter(
 }
 
 function isFilteredByAssignee(filter: Filter, issue: Issue): boolean {
-
   if (issue.issueOrPr === 'Issue') {
-    return (filter.assignees.some((assignee) => issue.assignees.includes(assignee))
-          || (filter.assignees.includes("Unassigned") && issue.assignees.length === 0));
-
-  } else if (issue.issueOrPr === 'PullRequest') { 
-    return (filter.assignees.some((assignee) => issue.author === assignee)
-          || (filter.assignees.includes("Unassigned") && issue.author === null)); 
-          // note that issue.author is never == null for PRs, but is left for semantic reasons
-
-  } else { // should never occur
+    return (
+      filter.assignees.some((assignee) => issue.assignees.includes(assignee)) ||
+      (filter.assignees.includes('Unassigned') && issue.assignees.length === 0)
+    );
+  } else if (issue.issueOrPr === 'PullRequest') {
+    return (
+      filter.assignees.some((assignee) => issue.author === assignee) || (filter.assignees.includes('Unassigned') && issue.author === null)
+    );
+    // note that issue.author is never == null for PRs, but is left for semantic reasons
+  } else {
+    // should never occur
     throw new Error('Issue or PR is neither Issue nor PullRequest');
   }
-   
 }
