1bd282dd132e3b4947dfa6fd75c2a82710254c70
wen xi <tngwenxi@gmail.com>
2025-03-20 01:08:34 +0800

    Add icon

--- src/app/shared/issue-pr-card/issue-pr-card-review-decision/issue-pr-card-review-decision.component.ts
+++ src/app/shared/issue-pr-card/issue-pr-card-review-decision/issue-pr-card-review-decision.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ReviewDecisionType } from '../../../core/models/issue.model';
+import { ReviewDecision } from '../../../core/models/issue.model';
 
 @Component({
   selector: 'app-issue-pr-card-review-decision',
@@ -12,4 +13,40 @@ export class IssuePrCardReviewDecisionComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  /**
+   * Returns the string for the relevant icon
+   */
+  getOcticon(): string {
+    switch (this.reviewDecision) {
+      case ReviewDecision.APPROVED:
+        return 'check';
+      case ReviewDecision.CHANGES_REQUESTED:
+        return 'file-diff';
+      case ReviewDecision.REVIEW_REQUIRED:
+        return 'dot-fill';
+    }
+  }
+
+  getOcticonColor(): string {
+    switch (this.reviewDecision) {
+      case ReviewDecision.APPROVED:
+        return '#57ab5a';
+      case ReviewDecision.CHANGES_REQUESTED:
+        return '#e5534b';
+      case ReviewDecision.REVIEW_REQUIRED:
+        return '#c69026';
+    }
+  }
+
+  reviewDecisionText() {
+    switch (this.reviewDecision) {
+      case ReviewDecision.APPROVED:
+        return 'Approved';
+      case ReviewDecision.CHANGES_REQUESTED:
+        return 'Changes requested';
+      case ReviewDecision.REVIEW_REQUIRED:
+        return 'Review required';
+    }
+  }
 }
