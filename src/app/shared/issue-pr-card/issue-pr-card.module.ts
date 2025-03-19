fe9c60bca181d1b3f938d5f4b82fd02ade2c5b71
wen xi <tngwenxi@gmail.com>
2025-03-19 22:19:15 +0800

    Display review decision in card

--- src/app/shared/issue-pr-card/issue-pr-card.module.ts
+++ src/app/shared/issue-pr-card/issue-pr-card.module.ts
@@ -4,10 +4,17 @@ import { IssuePrCardHeaderComponent } from './issue-pr-card-header/issue-pr-card
 import { IssuePrCardLabelsComponent } from './issue-pr-card-labels/issue-pr-card-labels.component';
 import { IssuePrCardMilestoneComponent } from './issue-pr-card-milestone/issue-pr-card-milestone.component';
 import { IssuePrCardComponent } from './issue-pr-card.component';
+import { IssuePrCardReviewDecisionComponent } from './issue-pr-card-review-decision/issue-pr-card-review-decision.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [IssuePrCardComponent, IssuePrCardHeaderComponent, IssuePrCardMilestoneComponent, IssuePrCardLabelsComponent],
+  declarations: [
+    IssuePrCardComponent,
+    IssuePrCardHeaderComponent,
+    IssuePrCardMilestoneComponent,
+    IssuePrCardLabelsComponent,
+    IssuePrCardReviewDecisionComponent
+  ],
   exports: [IssuePrCardComponent]
 })
 export class IssuesPrCardModule {}
