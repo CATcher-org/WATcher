f2e2e082a4d91524db78822a83dcebb6c96e709c
wen xi <tngwenxi@gmail.com>
2025-04-05 16:02:21 +0800

    Show tooltip if label overflows

--- src/app/shared/issue-pr-card/issue-pr-card-labels/issue-pr-card-labels.component.ts
+++ src/app/shared/issue-pr-card/issue-pr-card-labels/issue-pr-card-labels.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { GithubLabel } from '../../../core/models/github/github-label.model';
 import { LabelService } from '../../../core/services/label.service';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-issue-pr-card-labels',
@@ -10,5 +11,25 @@ import { LabelService } from '../../../core/services/label.service';
 export class IssuePrCardLabelsComponent {
   @Input() labels: GithubLabel[];
   @Input() labelSet: Set<string>;
+
+  tooltipLabel: string | null = null;
+
   constructor(public labelService: LabelService) {}
+
+  onMouseEnter(event: MouseEvent, labelName: string, tooltip: MatTooltip) {
+    const element = event.target as HTMLElement;
+    if (element.scrollWidth > element.clientWidth) {
+      this.tooltipLabel = labelName;
+
+      setTimeout(() => {
+        tooltip.show();
+      });
+    } else {
+      this.tooltipLabel = null;
+    }
+  }
+
+  onMouseLeave() {
+    this.tooltipLabel = null;
+  }
 }
