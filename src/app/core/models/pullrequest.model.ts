152a1c08af8fa38124f28a8a4ca8877b99bf9fac
YongJunXi <ernestyongjunxi@gmail.com>
2025-07-16 10:54:18 +0800

    Create pull request model

--- src/app/core/models/pullrequest.model.ts
+++ src/app/core/models/pullrequest.model.ts
@@ -1,18 +1,13 @@
-import * as moment from 'moment';
 import { GithubComment } from './github/github-comment.model';
 import { GithubIssue } from './github/github-issue.model';
-import { GithubLabel } from './github/github-label.model';
 import { HiddenData } from './hidden-data.model';
-import { Milestone } from './milestone.model';
+import { GithubLabel } from './github/github-label.model';
 import { PullrequestReview } from './pullrequest-review.model';
+import { ReviewDecision } from './issue.model';
+import moment from 'moment';
+import { Milestone } from './milestone.model';
 
-export enum ReviewDecision {
-  CHANGES_REQUESTED = 'CHANGES_REQUESTED',
-  APPROVED = 'APPROVED',
-  REVIEW_REQUIRED = 'REVIEW_REQUIRED'
-}
-
-export class Issue {
+export class PullRequest {
   /** Basic Fields */
   readonly globalId: string;
   readonly id: number;
@@ -39,6 +34,51 @@ export class Issue {
   reviews?: PullrequestReview[];
   reviewDecision?: ReviewDecision | null;
 
+  protected constructor(githubIssue: GithubIssue) {
+    /** Basic Fields */
+    this.globalId = githubIssue.id;
+    this.id = +githubIssue.number;
+    this.created_at = moment(githubIssue.created_at).format('lll');
+    this.updated_at = moment(githubIssue.updated_at).format('lll');
+    this.closed_at = moment(githubIssue.closed_at).format('lll');
+    this.title = githubIssue.title;
+    this.hiddenDataInDescription = new HiddenData(githubIssue.body);
+    this.description = PullRequest.updateDescription(this.hiddenDataInDescription.originalStringWithoutHiddenData);
+    this.state = githubIssue.state;
+    this.stateReason = githubIssue.stateReason;
+    this.issueOrPr = githubIssue.issueOrPr;
+    this.author = githubIssue.user.login;
+    // this.githubIssue = githubIssue;
+    this.isDraft = githubIssue.isDraft;
+    this.reviewDecision = githubIssue.reviewDecision;
+
+    this.assignees = githubIssue.assignees.map((assignee) => assignee.login);
+    this.githubLabels = githubIssue.labels;
+    this.labels = githubIssue.labels.map((label) => label.name);
+    this.milestone = githubIssue.milestone ? new Milestone(githubIssue.milestone) : Milestone.PRWithoutMilestone;
+    this.headRepository = githubIssue.headRepository?.nameWithOwner;
+    this.reviews = githubIssue.reviews?.map((review) => new PullrequestReview(review));
+  }
+
+  /**
+   * Processes and cleans a raw issue description obtained from user input.
+   */
+  static updateDescription(description: string): string {
+    const defaultString = 'No details provided by bug reporter.';
+    return PullRequest.orDefaultString(PullRequest.formatText(description), defaultString);
+  }
+
+  /**
+   * Given two strings, returns the first if it is not an empty string or a false value such as null/undefined.
+   * Returns the second string if the first is an empty string.
+   */
+  private static orDefaultString(stringA: string, def: string): string {
+    if (!stringA) {
+      return def;
+    }
+    return stringA.length !== 0 ? stringA : def;
+  }
+
   /**
    * Formats the text to create space at the end of the user input to prevent any issues with
    * the markdown interpretation.
@@ -62,67 +102,14 @@ export class Issue {
       return text;
     }
   }
-
-  /**
-   * Processes and cleans a raw issue description obtained from user input.
-   */
-  static updateDescription(description: string): string {
-    const defaultString = 'No details provided by bug reporter.';
-    return Issue.orDefaultString(Issue.formatText(description), defaultString);
-  }
-
-  /**
-   * Given two strings, returns the first if it is not an empty string or a false value such as null/undefined.
-   * Returns the second string if the first is an empty string.
-   */
-  private static orDefaultString(stringA: string, def: string): string {
-    if (!stringA) {
-      return def;
-    }
-    return stringA.length !== 0 ? stringA : def;
-  }
-
-  protected constructor(githubIssue: GithubIssue) {
-    /** Basic Fields */
-    this.globalId = githubIssue.id;
-    this.id = +githubIssue.number;
-    this.created_at = moment(githubIssue.created_at).format('lll');
-    this.updated_at = moment(githubIssue.updated_at).format('lll');
-    this.closed_at = moment(githubIssue.closed_at).format('lll');
-    this.title = githubIssue.title;
-    this.hiddenDataInDescription = new HiddenData(githubIssue.body);
-    this.description = Issue.updateDescription(this.hiddenDataInDescription.originalStringWithoutHiddenData);
-    this.state = githubIssue.state;
-    this.stateReason = githubIssue.stateReason;
-    this.issueOrPr = githubIssue.issueOrPr;
-    this.author = githubIssue.user.login;
-    // this.githubIssue = githubIssue;
-    this.isDraft = githubIssue.isDraft;
-    this.reviewDecision = githubIssue.reviewDecision;
-
-    this.assignees = githubIssue.assignees.map((assignee) => assignee.login);
-    this.githubLabels = githubIssue.labels;
-    this.labels = githubIssue.labels.map((label) => label.name);
-    this.milestone = githubIssue.milestone ? new Milestone(githubIssue.milestone) : Milestone.IssueWithoutMilestone;
-    this.headRepository = githubIssue.headRepository?.nameWithOwner;
-    this.reviews = githubIssue.reviews?.map((review) => new PullrequestReview(review));
-  }
-
-  public static createPhaseBugReportingIssue(githubIssue: GithubIssue): Issue {
-    return new Issue(githubIssue);
-  }
-
-  createGithubIssueDescription(): string {
-    return `${this.description}\n${this.hiddenDataInDescription.toString()}`;
-  }
 }
 
-export interface Issues {
-  [id: number]: Issue;
+export interface PullRequests {
+  [id: number]: PullRequests;
 }
 
-export const IssuesFilter = {
-  issuesViewer: {
+export const PullRequestsFilter = {
+  pullRequestsViewer: {
     Student: 'NO_FILTER',
     Tutor: 'NO_FILTER',
     Admin: 'NO_FILTER'
