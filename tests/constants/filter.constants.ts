514c40072732405621c316b4e07b47748fdc1806
Tan Wen Xuan <>
2025-08-14 17:14:51 +0800

    Update unit tests for FiltersService, LabelFilterBarComponent and constants in filters.constants.ts

--- tests/constants/filter.constants.ts
+++ tests/constants/filter.constants.ts
@@ -7,33 +7,22 @@ import {
   TypeFilter,
   TypeOptions
 } from '../../src/app/core/constants/filter-options.constants';
-import { Filter } from '../../src/app/core/services/filters.service';
+import { Filter } from '../../src/app/core/models/github/filters.model';
 
-export const DEFAULT_FILTER: Filter = {
-  title: '',
-  status: [StatusOptions.OpenPullRequests, StatusOptions.MergedPullRequests, StatusOptions.OpenIssues, StatusOptions.ClosedIssues],
-  type: TypeOptions.All,
-  sort: { active: SortOptions.Status, direction: OrderOptions.Asc },
-  labels: [],
-  milestones: [MilestoneOptions.PullRequestWithoutMilestone],
-  hiddenLabels: new Set<string>(),
-  deselectedLabels: new Set<string>(),
-  itemsPerPage: 20,
-  assignees: ['Unassigned']
-};
+export const DEFAULT_FILTER: Filter = Filter.createDefault();
 
-export const CHANGED_FILTER: Filter = {
+export const CHANGED_FILTER: Filter = Filter.createDefault().clone({
   title: 'test',
   status: [StatusOptions.OpenPullRequests],
   type: TypeOptions.Issue,
   sort: { active: SortOptions.Id, direction: OrderOptions.Asc },
   labels: ['aspect-testing', 'aspect-documentation'],
   milestones: ['V3.3.6'],
-  hiddenLabels: new Set<string>(['aspect-testing']),
-  deselectedLabels: new Set<string>(['aspect-documentation']),
+  hiddenLabels: new Set(['aspect-testing']),
+  deselectedLabels: new Set(['aspect-documentation']),
   itemsPerPage: 50,
   assignees: ['test']
-};
+});
 
 export const ENCODED_FILTER = {
   title: 'title',
